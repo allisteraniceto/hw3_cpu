1c12fea54bf98ad5f4874ec91b6e3f4089beaaba
allisteraniceto <allisteraniceto@gmail.com>
2022-11-02 17:58:29 -0700

    added base structure of program

--- cpu_speed.c
+++ cpu_speed.c
@@ -2,5 +2,11 @@
 #include <sys/time.h>
 
 int main(){
+    struct timeval current_time;
+    gettimeofday(&current_time, NULL);
+    for (int i = 0; i < 5; i++){
+
+    }
+    printf("seconds : %ld\nmicro seconds : %ld", current_time.tv_sec, current_time.tv_usec);
     return 0;
 }
